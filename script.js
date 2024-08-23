9859cc54653121e596b29c74d33c474aca025f87
mohamedyahyadk <154635643+mohamedyahyadk@users.noreply.github.com>
2024-08-23 08:42:37 -1100

    initializaation

---
+++ script.js
@@ -0,0 +1,6 @@
+const themeToggle = document.getElementById("theme-toggle");
+const body = document.body;
+
+themeToggle.addEventListener("change", () => {
+  body.classList.toggle("dark-theme");
+});
